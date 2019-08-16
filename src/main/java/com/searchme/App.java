95abc54572433098f53cccea2b74228a0fa7e85b
jspershen <jspershen@gmail.com>
2019-08-16 17:46:01 +0800

    execise

--- src/main/java/com/searchme/App.java
+++ src/main/java/com/searchme/App.java
@@ -4,10 +4,11 @@ package com.searchme;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println("nihao");
     }
 }
